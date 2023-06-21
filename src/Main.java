d4dce948fa7648453bded358515fce7025d391a3
Andymarch83 <5657031@gmail.com>
2023-06-21 20:37:59 +0300

    Readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,76 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] myStr;
+        ArrayList<String[]> arrayStrings = new ArrayList<>();
+        String encoding = System.getProperty("console encoding", "UTF-8");
+        Scanner scanner = new Scanner(System.in, encoding);
+
+        while (true) {
+            System.out.println(".".repeat(140));
+            System.out.println("Введите строки в формате: \nФамилия Имя Отчество Возраст Пол\nЧтобы закончить ввод данных - введите пустую строку.");
+            System.out.println("Введите стрроку: ");
+            myStr = GetArrayString(scanner.nextLine());
+            ArrayList<String> soname = new ArrayList<>();
+            ArrayList<String> name = new ArrayList<>();
+            ArrayList<String> s_name = new ArrayList<>();
+            ArrayList<Integer> age = new ArrayList<>();
+            ArrayList<String> sex = new ArrayList<>();
+            ArrayList<Integer> id = new ArrayList<>();
+
+
+            if (myStr == null)
+                break;
+
+            arrayStrings.add(myStr);
+            soname.add(myStr[0]);
+            name.add((myStr[1]));
+            s_name.add((myStr[2]));
+            age.add(Integer.valueOf(myStr[3]));
+            sex.add(myStr[4]);
+            id.add(arrayStrings.size());
+            System.out.println("Фамилия: " + soname);
+            System.out.println("Имя" + name);
+            System.out.println("Отчество: " + s_name);
+            System.out.println("Возраст: " + age);
+            System.out.println("Пол: " + sex);
+            System.out.println("Id" + id);
+
+        }
+
+
+
+
+        PrintArray(arrayStrings);
+    scanner.close();
     }
+
+    public static void PrintArray(ArrayList<String[]> arrayList) {
+       arrayList.sort(new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                int res = Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]);
+                return res;
+            }
+        });
+        for(String[] myStr: arrayList)
+        System.out.println(String.format("%s %s. %s. %s %s", myStr[0], myStr[1].charAt(0), myStr[2].charAt(0), myStr[3], myStr[4]));
+
+    }
+
+
+    public static String[] GetArrayString(String str) {
+        String[] result = null;
+        result = str.strip().split(" ");
+        if (result.length != 5)
+            return null;
+
+        return result;
+    }
+
+
 }
--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         ArrayList<String[]> arrayStrings = new ArrayList<>();
         ArrayList<String> fam = new ArrayList<>();
         ArrayList<String> name = new ArrayList<>();
@@ -11,32 +10,49 @@ public class Test {
         ArrayList<String> sex = new ArrayList<>();
         ArrayList<Integer> id = new ArrayList<>();
 
+        id.add(0);
+        id.add(1);
+        id.add(2);
+        id.add(3);
+        id.add(4);
+        id.add(5);
 
+        fam.add("Иванов");
+        fam.add("Семёнова");
+        fam.add("Васильков");
+        fam.add("Слепаков");
+        fam.add("Гаврикова");
+        fam.add("Филиппова");
 
-        while (true) {
-            System.out.println(".".repeat(140));
-            System.out.println("Введите строки в формате: \nФамилия Имя Отчество Возраст Пол\nЧтобы закончить ввод данных - введите пустую строку.");
-            String[] myList = GetArrayString(sc.nextLine());
-            arrayStrings.add(myList);
+        name.add("Иван");
+        name.add("Елена");
+        name.add("Павел");
+        name.add("Семён");
+        name.add("Вероника");
+        name.add("Катерина");
 
-            id.add(0,arrayStrings.size());
+        s_name.add("Семёнович");
+        s_name.add("Вячеславовна");
+        s_name.add("Андреевич");
+        s_name.add("Ушастович");
+        s_name.add("Маврикиевна");
+        s_name.add("Матвеевна");
 
-            fam.add(myList[0]);
+        age.add(67);
+        age.add(33);
+        age.add(34);
+        age.add(54);
+        age.add(28);
+        age.add(67);
 
-            name.add(myList[1]);
+        sex.add("муж");
+        sex.add("жен");
+        sex.add("муж");
+        sex.add("муж");
+        sex.add("жен");
+        sex.add("жен");
 
-            s_name.add(myList[2]);
-
-            age.add(Integer.valueOf(myList[3]));
-
-            sex.add(myList[4]);
-
-
-
-            if (myList == null)
-                break;
-
-            Collections.sort(id, new Comparator<Integer>() {
+        Collections.sort(id, new Comparator<Integer>() {
                 @Override
                 public int compare(Integer o1, Integer o2) {
                     return age.get(o1) - age.get(o2);
@@ -44,19 +60,9 @@ public class Test {
             });
 
             System.out.println(id);
-            id.forEach(n -> System.out.println(fam.get(n) + name.get(n) + s_name.get(n) + age.get(n) + sex.get(n)));
-            System.out.println(String.format("%s %s. %s. %s %s", myList[0], myList[1].charAt(0), myList[2].charAt(0), myList[3], myList[4]));
+            id.forEach(n -> System.out.println(fam.get(n) + " " + name.get(n) + " " + s_name.get(n) + " " + age.get(n) + " " + sex.get(n)));
+
         }
 
-
-    }
-    public static String[] GetArrayString(String str){
-        String[] result = null;
-        result = str.strip().split(" ");
-        if (result.length !=5) {
-            return null;
-        }
-        return result;
-    }
 
 }
